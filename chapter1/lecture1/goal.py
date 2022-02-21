4ae8d4f5209a6ea0786849f5f46031fe4672978f
HarrisonHutton <harrison.hutton6@gmail.com>
2022-02-21 18:20:32 -0500

    TransformFromCopy b and A

--- chapter1/lecture1/goal.py
+++ chapter1/lecture1/goal.py
@@ -1,11 +1,16 @@
 from manim import *
-config.background_color = "#191919"
+# config.background_color = "#191919"
+
+def set_background(self):
+    rect = FullScreenRectangle(fill_opacity = .5, stroke_width = 0).set_color([BLUE, PURPLE])
+    self.add(rect)
 
 class Goal(Scene):
     def construct(self):
-        goal = Text("Goal:", font_size=80)\
+        set_background(self)
+        goal = Text("Learning Goal:", font_size=80)\
             .to_corner(LEFT + UP)\
-            .set_color_by_gradient("#11DA28", "#4FE560")
+            .set_color_by_gradient("#11DA28", "#5AFF6D")
         self.play(Write(goal))
         self.wait(1.5)
 
@@ -14,17 +19,29 @@ class Goal(Scene):
             r"\usepackage{amsfonts}"
         )
 
-        product = MathTex(r"b=Ax", font_size = 140, color = BLUE).shift(UP*1.8)
-        to = MathTex(r"\to", font_size = 120, color = ORANGE).shift(LEFT*5.4)
+        product = MathTex(r"b=Ax", font_size = 140, color = RED).shift(UP*1.2)
+        to = MathTex(r"\to", font_size = 120, color = YELLOW).shift(LEFT*5.4 + DOWN*0.5)
 
         implication_1 = Tex(r"$b$", r" is a linear combination of", font_size = 80)
         implication_1.next_to(to, RIGHT)
-        implication_2 = Tex(r" the columns of ", r"$A$.", font_size = 80)
+        implication_2 = Tex(r" the columns of ", r"$A$", r".", font_size = 80)
         implication_2.next_to(to, DOWN, aligned_edge = LEFT)
         imp_group = VGroup(implication_1, implication_2)
-
 
         self.play(Write(product))
         self.play(Write(to))
         self.play(FadeIn(imp_group))
         self.wait()
+
+        # Now make b and A from Tex formula float into their
+        # positions in the implication.
+        move_b = product[0][0].copy()
+        og_b = implication_1[0].copy().set_color(RED)
+        move_A = product[0][2].copy()
+        og_A = implication_2[1].copy().set_color(RED)
+
+        self.play(
+            TransformFromCopy(move_b, og_b , path_arc = 80*DEGREES, run_time = 1.5),
+            TransformFromCopy(move_A, og_A , path_arc = 80*DEGREES, run_time = 1.5)
+        )
+        self.wait(3)
