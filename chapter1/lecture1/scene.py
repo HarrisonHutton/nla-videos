55b473a49e9b64125e3482e0ca18a85f5ef5d73a
HarrisonHutton <harrison.hutton6@gmail.com>
2022-02-22 23:34:21 -0500

    Migrate thumbnail code to thumbnail.py

--- chapter1/lecture1/scene.py
+++ chapter1/lecture1/scene.py
@@ -3,23 +3,6 @@ from manim import *
 config.background_color = "#191919"
 
 """
-Video thumbnail
-"""
-class Thumbnail(Scene):
-    def construct(self):
-        book = Text("Numerical Linear Algebra", font_size = 80)
-        title = Text("Matrix-Vector Multiplication", font_size = 54)
-        tex = MathTex(r"b=Ax", font_size = 280, color=GOLD).shift(DOWN*0.7)
-
-        book.set_color_by_gradient(TEAL, GREEN)
-        book.shift(UP*2.5)
-
-        title.set_color_by_gradient(RED, ORANGE)
-        title.shift(UP*1.4)
-
-        self.add(book, title, tex)
-
-"""
 Matrix-vector multiplication videos
 """
 class Goal(Scene):
